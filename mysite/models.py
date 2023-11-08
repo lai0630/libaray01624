61b69f8819d4441355118a02e3a506fb62d01548
lai0630 <11056024@ntub.edu.tw>
2023-11-08 15:14:38 +0800

    1108

--- mysite/models.py
+++ mysite/models.py
@@ -4,7 +4,14 @@ class Post(models.Model):
     slug = models.CharField(max_length=200)
     body = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
-
+    isBorrow = models.BooleanField(("外借中"),default=False)
+    
+    @property
+    def formatted_is_borrow(self):
+        if self.isBorrow:
+            return '<span style="color:white;background-color:red;border: 1px soild white; padding: 3px; border-radius: 3px;">外借中</span>'
+        else:
+            return '<span style="color:white;background-color:green;border: 1px soild white; padding: 3px; border-radius: 3px;">可借閱</span>'
     class Meta:
         ordering = ('-pub_date',)
 
--- mysite/views.py
+++ mysite/views.py
@@ -5,7 +5,8 @@ from datetime import datetime
 from django.shortcuts import redirect
 from django.shortcuts import render, redirect
 from django import forms as LoginForm
-
+from django.shortcuts import render,get_object_or_404,HttpResponseRedirect,reverse
+from django.utils.text import slugify
 
 def homepage(request):
     posts = Post.objects.all()
@@ -23,3 +24,20 @@ def showpost(request, slug):
 def log(request):
 	return render(request, 'login.html')
 	
+def borrow_book(request, book_id):
+    book = get_object_or_404(Post, id=book_id)
+
+    if not book.isBorrow:
+        book.isBorrow = True
+        book.save()
+
+    return HttpResponseRedirect(reverse('book_list'))
+
+def return_book(request, book_id):
+    book = get_object_or_404(Post, id=book_id)
+
+    if book.isBorrow:
+        book.isBorrow = False
+        book.save()
+
+    return HttpResponseRedirect(reverse('book_list'))
